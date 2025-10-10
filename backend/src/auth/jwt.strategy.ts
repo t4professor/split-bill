729d04f2d29f13c40dcf88884e2e8fd83a68fc1a
Tran Cao Bao Phuc <trancaobaophuc.work@gmail.com>
2025-10-10 10:02:52 +0700

    chore: reconfig gitignore
    changed jwt secret

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET || 'supersecret',
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET') || '',
     });
   }
 
   async validate(payload: any) {
+    // This method defines what will be attached to req.user
     return { userId: payload.sub, username: payload.username };
   }
 }
