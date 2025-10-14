e5190befa051957bac6c4cb5f9d5a00d714c14a9
Tran Cao Bao Phuc <trancaobaophuc.work@gmail.com>
2025-10-14 09:09:28 +0700

    feature: added /profile and use uuid instead of autoincreament id

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable, NotFoundException } from '@nestjs/comm
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
